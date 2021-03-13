4bcfe0120f7da04839fa2219220f8b7c0cd6d4c6
Shankhanil Ghosh <shankha.rik@gmail.com>
2021-03-13 19:55:46 +0530

    completed video-record module

--- src/components/VideoRecord/VideoRecord.js
+++ src/components/VideoRecord/VideoRecord.js
@@ -9,42 +9,70 @@ import { VideoRecordTexts } from '../../assets/ViewTexts/VideoRecordTexts';
 import { LanguageContext } from '../../context/LanguageContext';
 
 // firebase storage object
-import storage from '../../firebase';
+// import storage from '../../firebase';
 
 // material ui
 import { Button, IconButton } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        width: 400,
+        // height: 500,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+
+        'align-items': 'center',
+        'min-height': 200,
+    },
+    avatar: {
+        display: 'flex',
+        'justify-content': 'center',
+        // 'font-size': '80',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    }
+}));
 
 const VideoRecord = () => {
     // get the UI language
     let language = useContext(LanguageContext);
+    const classes = useStyles();
 
     // video recording state
     const [state, setState] = useState('beforeStarting');
     // reference to firebase storage
-    const storageRef = storage.ref();
+    // const storageRef = storage.ref();
 
-    // let uid, ageVal, genderVal;
+    const uploadingModal = (
+            <div
+                className={classes.paper}>
+                <h2 id="simple-modal-title">
+                    {VideoRecordTexts.uploadMsg[language.toString()]}
+                </h2>
+                {/* <Button onClick={() => { setState('uploadSuccess') }}> Record </Button> */}
+            </div>
+    );
 
-    const [annotation, setAnnotation] = useState({
-        uid: '0',
-        ageVal: '0',
-        genderVal: '',
-    });
+    const annotation = {
+        uid: localStorage.getItem('uid'),
+        ageVal: localStorage.getItem('ageVal'),
+        genderVal: localStorage.getItem('genderVal'),
+    };
+    const [uploadState, setUploadState] = useState(false);
 
     // hooks
     useEffect(() => {
-        const uid = localStorage.getItem('uid');
-        const ageVal = localStorage.getItem('ageVal');
-        const genderVal = localStorage.getItem('genderVal');
+        if (state === 'afterRecording') {
+            setUploadState(true);
+        }
+    }, [state]);
 
-        // console.log({uid, ageVal, genderVal}); 
-        // setAnnotation({...annotation, uid : uid,  });
-        setAnnotation({...annotation, uid : uid, ageVal : ageVal, genderVal : genderVal });
-        // setAnnotation({...annotation, ageVal : ageVal });
-        // setAnnotation({...annotation, genderVal : genderVal });
-        // setAnnotation(annotation => {uid: uid});
-    }, []);
 
     // function to execute after recording is complete
     const recoringComplete = (videoBlob) => {
@@ -54,6 +82,9 @@ const VideoRecord = () => {
          * create a reference to the new file
          * Generate filename for each person. 
          */
+        /**
+         * Un-comment this when connected to internet. 
+         * 
         const videoFile = `${annotation.uid}.mp4`;
         const annotationFile = `${annotation.uid}_annotation.txt`;
         
@@ -71,7 +102,8 @@ const VideoRecord = () => {
                 setState('uploadSuccess');
                 console.log('Uploaded annotation string!');
             });        
-        });
+        }); 
+        */
     }
 
     const ShowReadText = () => {
@@ -80,28 +112,34 @@ const VideoRecord = () => {
 
     return (
         <div>
-            
+
             <Link to='/'>
                 <IconButton>
                     <HomeIcon />
                 </IconButton>
             </Link>
-            {/* <p> {annotation.uid}, {annotation.ageVal}, {annotation.genderVal} </p> */}
+            <p> {annotation.uid}, {annotation.ageVal}, {annotation.genderVal} </p>
             <VideoRecorder
                 renderDisconnectedView={() => console.log('not connected')}
                 onRecordingComplete={videoBlob => recoringComplete(videoBlob)}
             />
             <ShowReadText />
-            {/* <Button onClick={() => {setState('afterRecording')}}> after </Button> */}
-            {/* <Button onClick={() => {setState('uploadSuccess')}}> succ?ess </Button> */}
+            <Button onClick={() => { setState('afterRecording') }}> Record </Button>
+            <h1> {uploadState.toString()} </h1>
+            <Modal
+                open={uploadState}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {uploadingModal}
+            </Modal>
             {
-                (state === 'uploadSuccess') ? 
-                <Redirect to={'/'} />
-                : < div />
+                (state === 'uploadSuccess') ?
+                    <Redirect to={'/'} />
+                    : < div />
             }
         </div>
     );
-
 }
 
 export default VideoRecord;
