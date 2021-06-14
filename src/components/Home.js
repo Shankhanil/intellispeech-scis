9df4cee6f5f08e84d0219e5095c55cbaad62cc6c
RittamDas <42467007+RittamDas@users.noreply.github.com>
2021-06-14 14:18:23 +0530

    Change in the app and routing structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './Home.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import AnnotationForm from './WelcomePage/AnnotationForm.js';
-import VideoRecord from  './VideoRecord/VideoRecord.js';
-import AudioRecord from './AudioRecord/AudioRecord.js';
 
 function Home() {
     return (
         <div className = 'home'>
-        <Router>
-            <Route exact path = '/'>
             <div className = 'home_text'>
                 SPEECH@SCIS<br/>
                 Welcomes You
@@ -22,20 +16,7 @@ function Home() {
                <Link to = '/vform' className = 'btn1'><button>Record your Video</button></Link>
                <Link to = '/aform' className = 'btn2'><button>Record your Audio</button></Link>
             </div>
-            </Route>
-            <Route exact path = '/vform'>
-                <AnnotationForm nextPage={'/video'} />
-            </Route>
-            <Route exact path = '/aform'>
-                <AnnotationForm nextPage={'/audio'} />
-            </Route>
-            <Route exact path = '/video'>
-                <VideoRecord />
-            </Route>
-            <Route exact path = '/audio'>
-                <AudioRecord />
-            </Route>
-        </Router>
+            
         </div>
     )
 }
